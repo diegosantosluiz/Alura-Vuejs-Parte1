4a687bd74faaeeeeeedc18b7eca9ec5215b6a996
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-30 12:51:58 -0300

    Aula 02 - Atividade 04: O poder das diretivas

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo" />
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,10 +14,16 @@ export default {
   data () {
     return {
       titulo: 'Alurapic',
-      foto: {
-        url: 'https://www.portaldodog.com.br/cachorros/wp-content/uploads/2017/01/3-doen%C3%A7as-que-todo-tutor-de-Dachshund-deve-prestar-mais-aten%C3%A7%C3%A3o-pdd1-500x357.jpg',
-        titulo: 'cachorro'
-      }
+      fotos: [
+        {
+          url: 'https://www.portaldodog.com.br/cachorros/wp-content/uploads/2017/01/3-doen%C3%A7as-que-todo-tutor-de-Dachshund-deve-prestar-mais-aten%C3%A7%C3%A3o-pdd1-500x357.jpg',
+          titulo: 'cachorro 1'
+        },
+        {
+          url: 'https://abrilsuperinteressante.files.wordpress.com/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
+          titulo: 'cachorro 2'
+        }
+      ]
     }
   }
 }
