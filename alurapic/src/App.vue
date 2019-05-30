549549f13bb4d2eb4f1cfe1bd26e25ea8ccc81fd
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-30 19:48:19 -0300

    Aula 03 - Atividade 05: Lifecycle Hooks: em que momento acessaremos nossa API?

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -14,17 +14,13 @@ export default {
   data () {
     return {
       titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'https://www.portaldodog.com.br/cachorros/wp-content/uploads/2017/01/3-doen%C3%A7as-que-todo-tutor-de-Dachshund-deve-prestar-mais-aten%C3%A7%C3%A3o-pdd1-500x357.jpg',
-          titulo: 'cachorro 1'
-        },
-        {
-          url: 'https://abrilsuperinteressante.files.wordpress.com/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
-          titulo: 'cachorro 2'
-        }
-      ]
+      fotos: [ ]
     }
+  },
+
+  created() {
+
+    alert('Criei os componentes');
   }
 }
 </script>
