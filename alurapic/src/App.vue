2577a00f071beec4fe0036b19593766a2c608466
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-30 19:59:13 -0300

    Aula 03 - Atividade 07: http e promises

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -19,8 +19,9 @@ export default {
   },
 
   created() {
-
-    alert('Criei os componentes');
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
