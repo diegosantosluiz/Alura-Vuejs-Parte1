6052f06c377cd4e8aea7504eb8a1fdb7b88e5d9d
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-30 20:12:13 -0300

    Aula 04 - Atividade 01: Estilizando um componente

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
         <img :src="foto.url" :alt="foto.titulo" />
       </li>
     </ul>
@@ -27,4 +27,23 @@ export default {
 </script>
 
 <style>
+  
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
 </style>
