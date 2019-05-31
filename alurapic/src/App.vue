f4a8a978d3f99902cf70bba3cbc836fbbc012fc1
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-30 21:53:27 -0300

    Aula 05 - Atividade 08: Que tal mais um componente?

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -6,7 +6,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
         </meu-painel>
 
       </li>
@@ -17,11 +17,13 @@
 <script>
 
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
 
   components: {
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   data () {
@@ -69,10 +71,6 @@ export default {
 
   .lista-fotos .lista-fotos-item {
     display: inline-block;
-  }
-
-  .imagem-responsiva {
-    width: 100%;
   }
 
   .filtro {
