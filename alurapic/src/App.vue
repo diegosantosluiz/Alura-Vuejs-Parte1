dd3084efcca0cab6fc7e3bc78cd99848bcf0d112
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-30 22:58:32 -0300

    Aula 06 - Atividade 09: Animando transições de página

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -9,7 +9,9 @@
       </ul>
     </nav>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -32,6 +34,13 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .4s;
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
   }
 
 </style>
